245740aa011f24cb32271f1f6b3312562582b5c5
Michał Jawulski <mjawulski@gmail.com>
2020-07-31 00:28:11 +0200

    bonds card initial design

--- src/app/bonds/bonds.module.ts
+++ src/app/bonds/bonds.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BondOverviewComponent } from './bond-overview/bond-overview.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [BondOverviewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [BondOverviewComponent],
 })
 export class BondsModule {}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,10 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule],
+  exports: [MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCardModule],
 })
 export class MaterialModule {}
